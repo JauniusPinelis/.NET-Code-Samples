821783faf610559ee1974ec81772d9d8214d40e3
JauniusPinelis <JauniusPinelis@gmail.com>
2021-08-02 20:56:06 +0300

    MsSql exaple

---
+++ MsSqlExample/MsSqlExample/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MsSqlExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MsSqlExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string connectionString = "Server=.;Database=ShopDb;Integrated Security=SSPI;";
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            connection.Open();
+
+            using var command = new SqlCommand("SELECT * FROM dbo.Items;", connection);
+            using var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                int id = reader.GetInt32(0);
+            }
+
+            connection.Close();
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
